bc54887e3f6cb49ab6cb0490dcc3a16fbf1841b5
yash <yashdhokane12@gmail.com>
2024-10-06 16:24:20 +0530

    second  commit

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const shortid = require("shortid");
 const validUrl = require("valid-url");
+const serverless = require("serverless-http");
+
+require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT ;
+ 
 
 mongoose
-  .connect( process.env.Mongo_url,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.Mongo_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
@@ -99,3 +101,4 @@ app.get("/api/url/:code", async (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
